9a745e663db5f3044eeddbb38078787e09f8f984
Your Name <williamputnamjr@gmail.com>
2018-02-07 20:28:47 -0800

    added a message bus not yet functioning

--- src/server/index.js
+++ src/server/index.js
@@ -10,6 +10,31 @@ var faker = require('faker');
 const app = new Koa();
 const PORT = process.env.PORT || 1337;
 
+// Amazon sqs set up
+const Consumer = require('sqs-consumer');
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+  region: 'us-east-2',
+  accessKeyId: 'AKIAIPD6YCKCILPDGMGA',
+  secretAccessKey: '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'
+});
+
+const queue = Consumer.create({
+  queueUrl: 'https://sqs.us-east-2.amazonaws.com/928047465876/Trending',
+  handleMessage: (message, done) => {
+    var msgBody = JSON.parse(message.body);
+    console.log('message', msgBody);
+    done();
+  },
+  sqs: new AWS.SQS()
+});
+
+queue.on('error', (err) => {
+  console.log(err.message);
+});
+
+queue.start();
 
 
 app.use(bodyParser());
--- src/server/routes/trending.js
+++ src/server/routes/trending.js
@@ -1,6 +1,53 @@
 const Router = require('koa-router');
 const queries = require('../db/queries/trending.js');
 const axios = require('axios');
+const config = require('./config.js');
+
+
+// redis cache set up
+let redis = require('redis');
+let client = redis.createClient(6379, {no_ready_check: true});
+
+client.auth((err) => {
+  if (err) console.log('error', err);
+});
+
+client.on('connect', () => {
+  console.log('Connected to Redis');
+});
+
+// Amazon sqs set up
+const Consumer = require('sqs-consumer');
+const AWS = require('aws-sdk');
+
+
+
+AWS.config.update({
+  region: 'us-east-2',
+  accessKeyId: config.keyId,
+  secretAccessKey: config.key
+});
+
+
+
+
+const queue = Consumer.create({
+  queueUrl: 'https://sqs.us-east-2.amazonaws.com/928047465876/Trending',
+  handleMessage: (message, done) => {
+    console.log('message', message.body);
+    done();
+  },
+  sqs: new AWS.SQS()
+
+});
+
+queue.on('error', (err) => {
+  console.log(err.message);
+});
+
+queue.start();
+
+const sqs = new AWS.SQS();
 
 const router = new Router();
 
@@ -15,6 +62,9 @@ router.get('/trending', async (ctx) => {
   try {
     const movies = await queries.getTrendingMovies(ctx.request.body.count);
     const num = ctx.request.body.count;
+    client.flushdb();
+    client.hmset(movies, [movies]);
+    client.hmget(movies);
     ctx.body = {
       status: 'success',
       data: {
@@ -27,14 +77,29 @@ router.get('/trending', async (ctx) => {
   }
 });
 
-
 router.post('/events', async (ctx) => {
+  console.log('ctx', ctx.request.body);
   try {
-    const video = await queries.updateMovies(ctx.request.body.data);
-    // axios.post('videos'); // sending to matt's service
+    const video = await queries.updateMovies(ctx.request.body);
+    var params = {
+      MessageBody: JSON.stringify(video),
+      QueueUrl: 'https://sqs.us-east-2.amazonaws.com/928047465876/Trending',
+
+    };
+    sqs.sendMessage(params, function(err, data) {
+      if (err) {
+        console.log('error', err);
+      }
+      else {
+        console.log('message', data, video);
+      }
+    });
     if (video.length) {
       ctx.body = {
-        status: 'success'
+        status: 'success',
+        data: {
+          video
+        }
       };
     } else {
       ctx.status = 404;
